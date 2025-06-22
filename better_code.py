a0ce94e9cf89daf0169f3dd5ebf4b7c6f74b2bef
Great-Szu <mateusz.chol.net@outlook.com>
2025-06-22 14:52:08 +0200

    Day 3, Free variables, difference between global, local, module scope. How to modify variables from higher scope and why not

---
+++ better_code.py
@@ -0,0 +1,6 @@
+def area_of_square(length: float):
+
+    return length * length
+
+
+print(f'The area is {area_of_square(30)}')
--- nested_function_1.py
+++ nested_function_1.py
@@ -1,12 +1,21 @@
 def greet_pythons(items: list) -> None:
     greeting = 'Hello'
+    print(f'The ID of "greeting" in "greet_pythons" is {id(greeting)}')
+    print(f'local namespace in "greet_pythons"(1): {locals()}')
 
     def make_greeting(item: str) -> str:
+        nonlocal greeting
+        print(f'local namespace in "make_greeting"(1): {locals()}')
+        greeting = 'Hi'
+        print(f'The ID of "greeting" in "make_greeting" is {id(greeting)}')
         return f' {greeting} {item}'
 
     for item in items:
         print(make_greeting(item))
+    print(f'Inside greet_pythons, "greeting" is {greeting}')
+    print(f'The ID of "greeting" in "greet_pythons" is {id(greeting)}')
+    print(f'local namespace in "greet_pythons"(2): {locals()}')
 
-names = ['John', 'Eric', 'Graham', 'Terry', 'Michael']
+names = ['John']#, 'Eric', 'Graham', 'Terry', 'Michael']
 
 greet_pythons(names)
